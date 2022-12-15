bb8af1e7d89a21a583aeb1b599f53a8284888b44
Lisa Snortheim <lsnortheim@merative.com>
2022-12-15 13:13:33 -0600

    Added several logging functions

--- module/log_helpers.py
+++ module/log_helpers.py
@@ -1,7 +1,34 @@
 ########################################
 # Functions to help with logging prints
 ########################################
+import os
+import datetime as dt
+import sys
+import time
 
+def setSysOut(logname):
+    log_file = open(f'{logname}','w')
+    sys.stdout = log_file
+    sys.stderr = log_file
+    printLogDetails
+
+def printLogDetails():
+    print('Initializing Log'.upper().center(72,'#'))
+    print('# User: ',os.getlogin())
+    print('# Date: ', dt.datetime.now())
+    print('#'*72)
 
 def printSectionHeader(section_nm):
-    print('*'*72,section_nm.upper.center, sep='/n')
+    print('*'*72,section_nm.upper().center(72, '*'),'*'*72, sep='/n')
+
+def printSectionSubHeader(subhead_nm):
+    print('-'*72,subhead_nm.upper().center(72, '-'), sep='/n')
+
+def printElapsedTime(start_time, statement='Elapsed Time: '):
+    elapsedTime = time.time() - start_time
+    minutes, seconds = divmod(elapsedTime,60)
+    hours, minutes = divmod(minutes, 60)
+    print(statement, 
+            f'{hours} hrs ' * (hours>0), 
+            f'{minutes} mins ' * (minutes>0), 
+            f'& {round(seconds,2)} secs.' * (seconds>0))
